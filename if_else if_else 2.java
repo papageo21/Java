305d457723fddca8204243ffffcc595fbba4dfc9
George <77338393+papageo21@users.noreply.github.com>
2021-12-27 19:59:35 +0200

    Create if_else if_else 2.java

---
+++ if_else if_else 2.java
@@ -0,0 +1,22 @@
+public class exercise4 {
+    public static void main(String[] args){
+        int number = 10;
+
+        if(number >= 70 && number <= 100)
+            System.out.println("A");
+        else if (number >= 60)
+            System.out.println("B");
+        else if(number >= 50)
+            System.out.println("C");
+        else if(number >= 40)
+            System.out.println("D");
+        else if(number >= 30)
+            System.out.println("E");
+        else if(number >=0)
+            System.out.println("F");
+        else
+            System.out.println("Error!!");
+
+    }
+
+}
