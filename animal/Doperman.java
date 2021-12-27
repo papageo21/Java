7e55b7d3a3fb03b94d3fef62727b1e3e92a50549
George <77338393+papageo21@users.noreply.github.com>
2021-12-27 20:04:15 +0200

    Create Doperman.java

---
+++ animal/Doperman.java
@@ -0,0 +1,10 @@
+class Doperman extends Dog {
+    public Doperman(int weight, int height, int db) {
+        super(weight, height, db);
+    }
+    public void run(){
+        System.out.println("Doperman running!!");
+    }
+
+
+}
