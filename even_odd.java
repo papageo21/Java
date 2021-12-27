5662a186b698ce2e0d26de606a856f2033ca2812
George <77338393+papageo21@users.noreply.github.com>
2021-12-27 19:19:32 +0200

    Create even_odd.java

---
+++ even_odd.java
@@ -0,0 +1,10 @@
+public class exercise {
+    public static void main(String[] args){
+        int x=10;
+
+        if(x % 2 == 0)
+            System.out.println("O "+ x +" einai artios arithmos");
+        else
+            System.out.println("O " + x + " einai peritos arithmos");
+    }
+}
